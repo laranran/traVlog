f0874de50608ee000c1c9bfc6b3e9af91575f511
kk5949 <kk5949@naver.com>
2018-05-28 09:41:17 +0900

    회원가입 및 로그인/로그인에러페이지 업로드 완료
    주의** pom.xml에 하나 추가했기 때문에 한번씩 체크해주세요

---
+++ src/main/java/mvc/dto/Member.java
@@ -0,0 +1,90 @@
+package mvc.dto;
+
+public class Member {
+
+	private String memid;
+	private String mempw;
+	private int memage;
+	private String memnick;
+
+	private String memsex;
+	private String memname;
+	private String mememail;
+	private String memphone;
+
+	public int getMemage() {
+		return memage;
+	}
+
+	public void setMemage(int memage) {
+		this.memage = memage;
+	}
+
+	public String getMemnick() {
+		return memnick;
+	}
+
+	public void setMemnick(String memnick) {
+		this.memnick = memnick;
+	}
+
+	public String getMemsex() {
+		return memsex;
+	}
+
+	public void setMemsex(String memsex) {
+		this.memsex = memsex;
+	}
+
+	public String getMemid() {
+		return memid;
+	}
+
+	public void setMemid(String memid) {
+		this.memid = memid;
+	}
+
+	public String getMempw() {
+		return mempw;
+	}
+
+	public void setMempw(String mempw) {
+		this.mempw = mempw;
+	}
+
+	public String getMemname() {
+		return memname;
+	}
+
+	public void setMemname(String memname) {
+		this.memname = memname;
+	}
+
+	public String getMememail() {
+		return mememail;
+	}
+
+	public void setMememail(String mememail) {
+		this.mememail = mememail;
+	}
+
+	public String getMemphone() {
+		return memphone;
+	}
+
+	public void setMemphone(String memphone) {
+		this.memphone = memphone;
+	}
+
+	public String toString() {
+		return "member=[ " 
+				+ "memid: " + memid 
+				+ ", mempw: " + mempw 
+				+ ", memnick: " + memnick
+				+ ", memsex: " + memsex
+				+ ", memage: " + memage
+				+ ", memname: " + memname 
+				+ ", mememail: " + mememail
+				+ ", memphone: " + memphone + " ]";
+	}
+}
---
+++ src/main/java/mvc/service/MemberService.java
@@ -0,0 +1,33 @@
+package mvc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mvc.dao.MemberDao;
+import mvc.dto.Member;
+
+@Service
+public class MemberService {
+	@Autowired MemberDao memberDao;
+	
+	//1이면 데이타 일치, 0이면 데이타 없음
+	public int memberCheck(Member member) {
+		return memberDao.memberCount(member);
+	}
+
+	public Member getMemberOne(Member member) {
+		return memberDao.getMemberOne(member);
+	}
+
+	public void createMember(Member member) {
+		memberDao.createMember(member);
+	}
+
+	public int idcheck(String memid) {
+		return memberDao.idcheck(memid);
+	}
+
+	public int nickcheck(String memnick) {
+		return memberDao.nickcheck(memnick);
+	}
+}
